bfa2541902cd6d3b4d3a20adb8c2c01d248da3f7
sisiliu-research <sisi.liu.research@gmail.com>
2024-11-01 13:21:35 +0100

    Update 02-bowtie2-a1.sh

--- bash_scripts/02-bowtie2-a1.sh
+++ bash_scripts/02-bowtie2-a1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #===========================================================================
-# on several sample using arrays
+# Alignment against taxonomic reference database 1 on several sample using arrays
 # Version 0.4
 #
 # by Sisi Liu
@@ -12,7 +12,7 @@
 # have to be modified by the user
 #=============================================================================
 
-#SBATCH --account=envi.envi
+#SBATCH --account=
 #SBATCH --job-name=l2a1
 #SBATCH --partition=smp
 #SBATCH --time=48:00:00
@@ -35,7 +35,7 @@ VIEW="samtools/1.16.1"
 
 # given variables
 #===================================================================
-SCRIPTSP="/path/to/scripts"
+SCRIPTSP="/path/to/scripts" # execute external python script: dedup_sam.py
 WORK="/path/to/project/output/directory"
 OUTDIR="output-01-18"
 OUT_FASTP="out.dedupe"
@@ -61,9 +61,9 @@ module load ${VIEW}
 mkdir -p ${WORK}/${OUT_ALIGN}
 mkdir -p ${WORK}/${OUT_ALIGN}/${FILEBASE}
 
-#== archaea, fungi, viral
+#== archaea, fungi, viral refseq
 # which path 
-DBP="/albedo/work/projects/p_biodiv_dbs/bowtie2_ngsLCA/bowtie2_db_23_08"
+DBP="/path/to/p_biodiv_dbs/bowtie2_ngsLCA/bowtie2_db_23_08"
 # wich DB
 DBi="afv/afv"
 # full path to DB
@@ -74,7 +74,8 @@ srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal
 srun samtools view -bS ${SAMF}/${FILEBASE}.$(basename $DB).sam > ${SAMF}/${FILEBASE}.$(basename $DB).bam
 rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 
-#== whih DB?
+#== Bacteria refseq
+# whih DB?
 for DB in ${DBP}/bac00/bac.?; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -82,7 +83,7 @@ for DB in ${DBP}/bac00/bac.?; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+# whih DB?
 for DB in ${DBP}/bac01/bac.?; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -90,7 +91,7 @@ for DB in ${DBP}/bac01/bac.?; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+# whih DB?
 for DB in ${DBP}/bac02/bac.??; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -98,7 +99,7 @@ for DB in ${DBP}/bac02/bac.??; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+# whih DB?
 for DB in ${DBP}/bac03/bac.??; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -106,6 +107,7 @@ for DB in ${DBP}/bac03/bac.??; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
+# whih DB?
 for DB in ${DBP}/bac04/bac.??; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -113,6 +115,7 @@ for DB in ${DBP}/bac04/bac.??; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
+# whih DB?
 for DB in ${DBP}/bac05/bac.??; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -120,6 +123,7 @@ for DB in ${DBP}/bac05/bac.??; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
+# whih DB?
 for DB in ${DBP}/bac06/bac.??; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -127,14 +131,15 @@ for DB in ${DBP}/bac06/bac.??; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
+# whih DB?
 for DB in ${DBP}/bac07/bac.??; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
 	srun samtools view -bS ${SAMF}/${FILEBASE}.$(basename $DB).sam > ${SAMF}/${FILEBASE}.$(basename $DB).bam
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
-
-#== whih DB?
+#== plant, protozoa, plastid, mitochondrion refseq
+# whih DB?
 for DB in ${DBP}/pppm/pppm.{1..4}; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -142,6 +147,7 @@ for DB in ${DBP}/pppm/pppm.{1..4}; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
+# remove duplicates: share refseq between plant and mitochondrion package
 FILE_IN="${FILEBASE}.pppm.5.sam"
 FILE_OUT="${FILEBASE}.pppm.5.dedup.sam"
 for DB in ${DBP}/pppm/pppm.5; do\
@@ -152,7 +158,8 @@ for DB in ${DBP}/pppm/pppm.5; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+#== Vertebrate mammalian
+# whih DB?
 for DB in ${DBP}/vert_mam00/vert_mam.?; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -160,7 +167,7 @@ for DB in ${DBP}/vert_mam00/vert_mam.?; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+# whih DB?
 for DB in ${DBP}/vert_mam01/vert_mam.?; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -168,7 +175,7 @@ for DB in ${DBP}/vert_mam01/vert_mam.?; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+# whih DB?
 for DB in ${DBP}/vert_mam02/vert_mam.??; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -176,7 +183,8 @@ for DB in ${DBP}/vert_mam02/vert_mam.??; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+#== Vertebrate other
+# whih DB?
 for DB in ${DBP}/vert_other00/vert_other.?; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -184,7 +192,7 @@ for DB in ${DBP}/vert_other00/vert_other.?; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+# whih DB?
 for DB in ${DBP}/vert_other01/vert_other.?; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -192,7 +200,7 @@ for DB in ${DBP}/vert_other01/vert_other.?; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== whih DB?
+# whih DB?
 for DB in ${DBP}/vert_other02/vert_other.?; do\
 	echo Mapping ${OUT_MERGED} against $DB
 	srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
@@ -200,25 +208,27 @@ for DB in ${DBP}/vert_other02/vert_other.?; do\
 	rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 done
 
-#== others
+#== Ancient mammalian
 # which path 
-DBP="/albedo/work/projects/p_biodiv_dbs/bowtie2_ngsLCA/build"
+DBP="/path/to/p_biodiv_dbs/bowtie2_ngsLCA/build"
 # wich DB
 DBi="ancient_mammals/ancient_mammals"
 # full path to DB
 DB="${DBP}/${DBi}"
 
-#== mapping
+# mapping
 echo Mapping ${OUT_MERGED} against $DB
 srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
 srun samtools view -bS ${SAMF}/${FILEBASE}.$(basename $DB).sam > ${SAMF}/${FILEBASE}.$(basename $DB).bam
 rm ${SAMF}/${FILEBASE}.$(basename $DB).sam
 
+#== Chloroplast genome from Stefano Meucci paper
 # wich DB
 DBi="chloroplast_stefano/chloroplast_stefano"
 # full path to DB
 DB="${DBP}/${DBi}"
-#== mapping
+
+# mapping
 echo Mapping ${OUT_MERGED} against $DB
 srun bowtie2 --threads ${CPU} -k 1000 -x $DB -U ${INDIR}/${OUT_MERGED} --no-unal -S ${SAMF}/${FILEBASE}.$(basename $DB).sam
 srun samtools view -bS ${SAMF}/${FILEBASE}.$(basename $DB).sam > ${SAMF}/${FILEBASE}.$(basename $DB).bam
