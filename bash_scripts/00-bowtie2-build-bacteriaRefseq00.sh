6291350ece1e67ae6e6706c13da7323f63c24e02
sisiliu-research <sisi.liu.research@gmail.com>
2025-01-15 16:02:33 +0100

    Create 00-bowtie2-build-bacteriaRefseq00.sh

---
+++ bash_scripts/00-bowtie2-build-bacteriaRefseq00.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#========
+# slurm to build taxonomic reference database
+# taking /bac00 as an example, including /bac00/bac.{1..9}
+
+#========
+
+#SBATCH --account=
+#SBATCH --job-name=b0in
+#SBATCH --partition=fat
+#SBATCH --time=48:00:00
+#SBATCH --qos=48h
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=350G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=
+
+module load bowtie2/2.5.1
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+
+cd /path/to/bowtie2db/bac00
+
+for file in bac.?; do\
+  srun bowtie2-build --threads ${SLURM_CPUS_PER_TASK} $file $file
+done
